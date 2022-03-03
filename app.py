8ff8a2261e322e589416aae964618404bafb00cb
Aman Chauhan <66059307+Aman-web2000@users.noreply.github.com>
2022-03-03 14:51:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np 
+import plotly.express as px 
+from PIL import Image
+
+
+
+df=pd.read_csv(r"fitness_exercises.csv")
+
+df.drop('id',axis=1,inplace=True)
+
+df.rename({'bodyPart':"Body_Part", 'target':'Target_Muscle','name':'Exercise_Name','gifUrl':'Exercise_gif','equipment':"Equipment"},axis=1,inplace=True)
+
+
+## Side Bar
+html_sidebar_heading="""
+<div style='background-color:#0b2038;'>
+<h1 style="color: white; text-align: center">Find The Exercise</h1>
+</div>
+"""
+
+st.sidebar.markdown(html_sidebar_heading,unsafe_allow_html=True)
+
+body_part=st.sidebar.selectbox('Body Part',df['Body_Part'].unique())
+
+
+
+target_muscle=st.sidebar.selectbox('Muscle',df[df['Body_Part']==body_part]['Target_Muscle'].unique())
+
+
+if st.sidebar.button('Go'):
+    #ex=(
+    #    df[(df['Body_Part']==body_part) & (df['Target_Muscle']==target_muscle)]['Exercise_Name'].values
+    #)
+
+    #ex_bar=px.bar(ex,width=800,height=700,title="<b>Exercise Name for Your Muscle Group<b>")
+    #st.plotly_chart(ex_bar)
+    #st.write(ex)
+    gif_url=(
+        df[(df['Body_Part']==body_part) & (df['Target_Muscle']==target_muscle)]['Exercise_gif'].values
+    )
+    for i in gif_url:
+        ex=df[df['Exercise_gif']==i]['Exercise_Name'].values[0]
+        st.subheader(ex)
+        st.image(i)
+
+
+
+## Main Page 
+
+def main():
+    html_sidebar_heading="""
+    <div style='background-color:#010101;'>
+    <h1 style="color: yellow; text-align: center; font-size:50px">Fitness</h1>
+    </div>
+    """
+
+    st.markdown(html_sidebar_heading,unsafe_allow_html=True)
+
+
+    html_about="""<div style='font-color:white; padding: 10px;'>
+    </div>"""
+
+    st.markdown(html_about,unsafe_allow_html=True)
+    
+    st.header("Analytics")
+
+    a=st.selectbox("Select a Feature",df.columns)
+    b=st.selectbox("Select Feature to compare",df.columns[1:])
+    bb=df.groupby(b).apply(lambda df:df[a].value_counts().sum()).sort_values(ascending=False).head(15)
+    bb_px=px.bar(bb,title=f'Top 15 Most Used {b} by {a}')
+    bb_px
+
+
+
+if __name__=="__main__":
+    main()
